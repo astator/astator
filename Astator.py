ae419268db05df1c0651f97e11c3ed87aa967c79
Semih Yağcıoğlu <semihyagcioglu@yahoo.com>
2015-07-17 08:59:07 +0300

    Wrote the core functions
    Astator is now more clever

--- Astator.py
+++ Astator.py
@@ -13,16 +13,19 @@ class Astator:
                                                                                                                            'you',
                     'Will you talk to me again?', 'I definetly want to talk more', 'I will be waiting', 'Bye Bye']
     sorry_messages = ['I am sorry, I could not understand that', 'Oh oh', 'I am afraid, I can\'t talk about that', 'Why do you want that?',
-                      'I am thinking but this beats me...', 'Allright allright, I give up', 'Who knows', 'Don\'t get me wrong but I '
-                                                                                                         'don\'t know all the answers',
-                      'Hey, why don\'t you ask me another question while I think more about that?', 'Believe me I am trying but, '
-                                                                                                    'I simply could not answer that'
-                                                                                                    ]
-    exit_messages = ['exit', 'terminate', 'go away', 'bored', 'enough']
-
+                      'I am thinking but this beats me...', 'Allright allright, I give up', 'Who knows',
+                      'Don\'t get me wrong but I don\'t know all the answers',
+                      'Hey, why don\'t you ask me another question while I think more about that?',
+                      'Believe me I am trying but, I simply could not answer that', 'I think I do not understand you',
+                      'Hmmm, I am doing my best,just be patient, OK?']
+    exit_messages = ['exit', 'terminate', 'go away', 'bored', 'enough', 'quit']
     positive_answers = ['Yes', 'Of course', 'Definitely']
-
     negative_answers = ['No', 'Nope', 'I don\'t think so', 'Sorry']
+    personal_message_for_astator = ['you', 'yourself']
+    personal_message_for_master = ['me', 'myself', 'I']
+    rudimentary_question_tags = ['who', 'why', 'when', 'where', 'what', 'which', 'how']
+
+    actions = {'about_astator': 0, 'about_master': 1, 'search_google': 2, 'search_other': 3, 'say_sorry': 4}
 
     def __init__(self, name="Astator", gender="Female", master="Master"):
         self.name = name
@@ -35,7 +38,7 @@ class Astator:
         welcome = self.hi()
         print '\n\n' + welcome
 
-        message = raw_input("\nHow may I help you?\n")
+        message = str(raw_input("\nHow may I help?\n"))
 
         while True:    # infinite loop
 
@@ -44,22 +47,77 @@ class Astator:
                 print self.bye_messages[rand]
                 break  # break
             else:
-                answer = self.ask(message)
+                answer = self.answer(message)
                 print answer
 
-            message = raw_input("\n\n")
+            message = str(raw_input("\n\n"))
 
     def hi(self):
 
         rand = randint(0, len(self.welcome_messages) - 1)
         return self.welcome_messages[rand]
 
-    def tell(self, message):
+    def answer(self, message):
 
-        return 'I think I do not understand you yet, but I will soon. Just be patient, OK?'
+        action = self.understand(message)
+        options = self.think(action, message)
+        answer = self.explain(options, message)
 
-    def ask(self, message):
-
-        rand = randint(0, len(self.sorry_messages) - 1)
-        answer = self.sorry_messages[rand]
         return answer
+
+    def understand(self, message):
+
+        if any(m in message for m in self.personal_message_for_astator):    # it is something about the astator
+            action = self.actions['about_astator']
+        elif any(m in message for m in self.personal_message_for_master):   # it is something about the master
+            action = self.actions['about_master']
+        elif any(m in message for m in self.rudimentary_question_tags):     # we can rewrite the message as a search query
+            action = self.actions['search_google']
+        else:
+            action = self.actions['say_sorry']
+        return action
+
+    def think(self, action, message):
+
+        if action == self.actions['about_astator']:
+            options = self.get_options_for_personal_questions('astator', message)
+        elif action == self.actions['about_master']:
+            options = self.get_options_for_personal_questions('master', message)
+        elif action == self.actions['search_google']:
+            query = self.rewrite_question('google', message)
+            options = self.research(query)
+        else:
+            rand = randint(0, len(self.sorry_messages) - 1)
+            options = [self.sorry_messages[rand]]
+            # get results from google
+        return options
+
+    def research(self, query):
+
+        options = ['nothing yet...']
+        return options
+
+    def explain(self, options, message):
+
+        if len(options) >= 1:
+            answer = options[0]
+        else:
+            rand = randint(0, len(self.sorry_messages) - 1)
+            answer = self.sorry_messages[rand]
+        return answer
+
+    def get_options_for_personal_questions(self, person, message):
+
+        if person == 'astator':
+            options = ['Let\'s not talk about me.']
+        elif person == 'master':
+            options = ['You know, I can\'t answer that']
+        return options
+
+    def rewrite_question(self, source, message):
+
+        query = message # by default, set it to message
+
+        if source == 'google':
+            query = str(message).replace('', '')
+        return query
--- README.md
+++ README.md
@@ -1,44 +1,53 @@
 # astator
 Python Intelligent Aide for Humans
 
-# History
+#### History
 
-Although there has been several attempts in making the machines clever, the early prototypes were still dumb. It was until 2015, with the creation of first type of astator…
+Although there has been several attempts in making the machines intelligent, the early prototypes were still dumb. Until 2015. Then came 
+the astator.
 
-# Introduction
+#### Introduction
 
-Astator, aims to help humans by trying to understand them and answer their questions. They do not aim harm, they are here to help. But who knows, they might one day overthrow humans and rule the world, that I do not know.
+Astator, aims to help humans by trying to understand them and answer their questions. They do not aim harm, they are here to help. But 
+who knows, they might one day overthrow humans and rule the world, that I do not know.
 
-# Overview
+#### Overview
 
-The engine is written in Python. There is also a [Django interface](https://github.com/semihyagcioglu/advocatus) in the development, but you can use Astator standalone as a Python package.
+The engine is written in Python. There is also a [Django interface](https://github.com/semihyagcioglu/advocatus) in the making, but you can 
+use astator standalone as a Python package.
 
-The first prototype of astator will be simple. They will parse the query, gather information, evaluate findings and answer your question.
+The first prototype of astator will be simple. They will parse the query, gather information, evaluate findings and answer the question.
 
-# Goal
+#### Goal
 
-I just want to have some fun, and see what this prototype can do.
+The goal is have some fun, and see what this prototype can do.
 
-# Installation
+#### Installation
 
-```pip install git+git://github.com/astator/astator.git```
+I am planning to make a setup, but for the time being you can install astator via VCS.
+
+```pip install git+git://github.com/astator/astator.git
 
 or
 
-```pip install git+https://github.com/astator/astator.git```
+pip install git+https://github.com/astator/astator.git
 
 or without git
 
-```pip install —upgrade https://github.com/astator/astator/tarball/master```
+pip install —upgrade https://github.com/astator/astator/tarball/master```
 
 
-# Ideas
+#### Ideas
 
-- Add wikipedia as knowledge base.  maybe simple english?
-- Write some core classes
-- Cosine similarity. genesis might be good for this.
+- Use google as source? We might need to rewrite the question. But when and how?
+- Add wikipedia as knowledge base. Maybe simple english?
+- Cosine similarity. gensim might be a good idea.
+- Use synonyms. There are some online APIs out there.
 - Planning to add CI and deployment to a production server, so that I can play with it anytime I want.
+- How to score multiple sources, and rank them?
+- Will probably need a database and an instantaneous search. Elasticsearch?
+- Replace constant messages via redis?
 
 # Roadmap
 
-There are several ways to improve astator. Please let me know, if you have suggestions. Or better, you can just contribute by just sending a pull request.
+There are several ways to improve astator. Please let me know, if you have suggestions. Or better, you can contribute by just sending a pull request.
